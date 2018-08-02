1920c1f0fc1d242deff576169822c60c2499600c
Mahmoud Elmadah <mahmoud.sayed235@gmail.com>
2018-08-02 19:04:30 +0300

    last update

---
+++ Assets/openCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class openCamera : MonoBehaviour {
+
+	// Use this for initialization
+
+	WebCamTexture mCamera = null;
+	public GameObject plane;
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("Script has been started");
+
+		mCamera = new WebCamTexture ();
+		plane.GetComponent<MeshRenderer> ().material.mainTexture = mCamera;
+		plane.transform.localScale = new Vector3 (mCamera.width*75,1,mCamera.height*75);
+		mCamera.Play ();
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/updatePosition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updatePosition : MonoBehaviour {
+	private float longitude,latitude;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StartLocationService());
+	}
+	private IEnumerator StartLocationService(){
+		while (true) {
+			if (!Input.location.isEnabledByUser) {
+				print ("gps is closed");
+				yield break;
+			}
+			Input.location.Start ();
+			int max = 1;
+			while (Input.location.status == LocationServiceStatus.Initializing && max > 0) {
+				yield return new WaitForSeconds (1);
+				max--;
+			}
+			if (max == 0) {
+				print ("timed out");
+				yield break;
+			}
+			if (Input.location.status == LocationServiceStatus.Failed) {
+				yield break;
+			} 
+			longitude = (Input.location.lastData.longitude%0.1F)*100000;
+			latitude = (Input.location.lastData.latitude%0.1F)*100000;
+			gameObject.transform.localPosition = new Vector3 (longitude , this.transform.position.y, latitude);
+		
+			Input.location.Stop();
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
